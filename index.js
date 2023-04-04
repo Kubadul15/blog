1f18d20691e8ac466dfc910928e0b97f58a01727
JestemKamil <84380834+JestemKamil@users.noreply.github.com>
2023-04-04 05:18:26 +0000

    utworzenie bazy danych

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const app = express()
 const db = new sqlite3.Database("database.db")
 
 db.exec(`CREATE TABLE IF NOT EXISTS Users (
-    id NUMBER,
-    name TEXT
+    id NUMBER AUTO_INCREMENT not null,
+    name TEXT not null,
+    password TEXT not null
+);
+CREATE TABLE IF NOT EXISTS Posts (
+    id NUMBER AUTO_INCREMENT not null,
+    author TEXT not null,
+    title TEXT not null,
+    content TEXT not null
+
 );`)
 app.set("view engine", "ejs")
 app.get("/", (req,res) => {
@@ -20,4 +28,4 @@ app.get("/posts", (req,res) => {
     res.render("posts")
 })
 
-app.listen(3000)
+app.listen(3000, () =>console.log("Aplikacja działa na porcie 3000"))
