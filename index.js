4183f30bd7f25df5420241591d25fbb3bd6d8f48
JestemKamil <84380834+JestemKamil@users.noreply.github.com>
2023-04-04 05:05:14 +0000

    Podstawkaw=

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ const sqlite3 = require("sqlite3")
 const app = express()
 const db = new sqlite3.Database("database.db")
 
+db.exec(`CREATE TABLE IF NOT EXISTS Users (
+    id NUMBER,
+    name TEXT
+);`)
 app.set("view engine", "ejs")
 app.get("/", (req,res) => {
     res.render("index")
+})
+
+app.get("/users", (req,res) => {
+    res.render("users")
+})
+
+app.get("/posts", (req,res) => {
+    res.render("posts")
 })
 
 app.listen(3000)
