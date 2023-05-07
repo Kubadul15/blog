66b154001812b6d9bd68c45608c044b8d6777074
JestemKamil <kamilwasik15@gmail.com>
2023-05-07 18:29:52 +0200

    Dodano system autoryzacji(pin)

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ const express = require("express")
 const sqlite3 = require("sqlite3")
 const app = express()
 const db = new sqlite3.Database("database.db")
-
-db.exec(`CREATE TABLE IF NOT EXISTS Users (
-    id INTEGER  PRIMARY KEY,
-    name TEXT not null,
-    password TEXT not null
-);
+const pin = 2137
+db.exec(`
 CREATE TABLE IF NOT EXISTS Posts (
     id INTEGER  PRIMARY KEY,
     author TEXT not null,
@@ -21,7 +17,7 @@ app.set("view engine", "ejs")
 app.use(express.urlencoded({extended: false}))
 
 app.get("/", (req,res) => {
-    const posts = db.all(`SELECT * FROM Posts`, (err, rows) =>{
+    const posts = db.all(`SELECT * FROM Posts ORDER BY id DESC`, (err, rows) =>{
         if(err){
             console.log(err)
         }
@@ -33,13 +29,20 @@ app.get("/newpost", (req,res) => {
     res.render("newpost")
 })
 
+app.get("/error/pin", (req,res) =>{
+    res.render("errorpin")
+})
 app.post("/newpost/submit", (req,res) =>{
+    if(req.body.pin != pin){
+        res.redirect("/error/pin")
+    }else{
     const author = req.body.author
     const title = req.body.title
     const content = req.body.content
     db.all(`INSERT INTO Posts(author, title, content)
     VALUES('${author}', '${title}', '${content}');`)
     res.redirect("/")
-})
+    }})
+
 
 app.listen(3000, () =>console.log("Aplikacja działa na porcie 3000"))
