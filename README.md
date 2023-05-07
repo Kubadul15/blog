80860e24635a9052612896bab0afafebe6b103f6
JestemKamil <kamilwasik15@gmail.com>
2023-05-07 17:33:27 +0200

    Dodanie funkcji dodawania, odczytu postów z DB

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# cms
+# System bloga napisany w node.js
+
+Instalacja:
+
+1. git clone https://github.com/JestemKamil/blog
+2. npm install
+3. node index.js
--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = express()
 const db = new sqlite3.Database("database.db")
 
 db.exec(`CREATE TABLE IF NOT EXISTS Users (
-    id NUMBER AUTO_INCREMENT not null,
+    id INTEGER  PRIMARY KEY,
     name TEXT not null,
     password TEXT not null
 );
 CREATE TABLE IF NOT EXISTS Posts (
-    id NUMBER AUTO_INCREMENT not null,
+    id INTEGER  PRIMARY KEY,
     author TEXT not null,
     title TEXT not null,
     content TEXT not null
@@ -18,8 +18,28 @@ CREATE TABLE IF NOT EXISTS Posts (
 
 
 app.set("view engine", "ejs")
+app.use(express.urlencoded({extended: false}))
+
 app.get("/", (req,res) => {
-    res.render("index")
+    const posts = db.all(`SELECT * FROM Posts`, (err, rows) =>{
+        if(err){
+            console.log(err)
+        }
+        res.render("index", {posts: rows})
+    })
+})
+
+app.get("/newpost", (req,res) => {
+    res.render("newpost")
+})
+
+app.post("/newpost/submit", (req,res) =>{
+    const author = req.body.author
+    const title = req.body.title
+    const content = req.body.content
+    db.all(`INSERT INTO Posts(author, title, content)
+    VALUES('${author}', '${title}', '${content}');`)
+    res.redirect("/")
 })
 
 app.listen(3000, () =>console.log("Aplikacja działa na porcie 3000"))
